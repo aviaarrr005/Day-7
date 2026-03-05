0c839cf56fec22c11261b4b7460cc5ab41525085
aviaarrr005 <avilashrout2005@gmail.com>
2026-03-05 09:44:39 +0530

    Day 7 Prog 1

---
+++ TerminatedState.java
@@ -0,0 +1,24 @@
+class MyThread2 extends Thread
+{
+    public void run()
+    {
+        System.out.println("Thread Finished");
+    }
+}
+public class TerminatedState
+{
+    public static void main(String args[])
+    {
+        MyThread2 t1 = new MyThread2();
+        t1.start();
+        try
+        {
+            t1.join();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Thread Interrupted: " + e.getMessage());
+        }
+        System.out.println("State After Termination: " + t1.getState());
+    }
+}
